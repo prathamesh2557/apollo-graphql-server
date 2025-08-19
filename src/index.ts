3e6f9d01cdfb4c2f40ed765ccebc3abaa736ce44
Pratham <prathamesh2557@gmail.com>
2025-08-19 20:10:14 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 import { SESSION_SECRET } from './constants';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'; 
 import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/postgresql';
 import mikroConfig from './mikro-orm.config';
