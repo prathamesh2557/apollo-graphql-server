7ccccd6fe1d396d4ee133305c12971ca8dd9c417
Pratham <prathamesh2557@gmail.com>
2025-12-17 23:39:12 +0530

    popped out of nowhere

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const __prod__ = process.env.NODE_ENV === 'production';
 export const COOKIE_NAME = 'cookie name';
-export const SESSION_SECRET = 'session secret';
+export const SESSION_SECRET = 'session secret'; 
